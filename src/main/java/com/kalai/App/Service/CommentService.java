18ea82155ef0d0b75cfcfd11e7f52d85314f2d69
kalai <theridingart@gmail.com>
2021-12-20 16:34:27 +0530

    created comments repo interface

--- src/main/java/com/kalai/App/Service/CommentService.java
+++ src/main/java/com/kalai/App/Service/CommentService.java
@@ -1,4 +1,13 @@
 package com.kalai.App.Service;
 
+import com.kalai.App.Entity.Comments;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@Component
 public class CommentService {
+
+    public void saveComment(String comment){
+        Comments comments=
+    }
 }
