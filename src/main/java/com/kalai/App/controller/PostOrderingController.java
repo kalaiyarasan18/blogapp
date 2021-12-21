3b165ecbd44d067a0498296fa8f1e8c4c7599d0f
kalai <theridingart@gmail.com>
2021-12-21 15:52:33 +0530

    Added sorting methods

--- src/main/java/com/kalai/App/controller/PostOrderingController.java
+++ src/main/java/com/kalai/App/controller/PostOrderingController.java
@@ -1,4 +1,33 @@
 package com.kalai.App.controller;
 
+import com.kalai.App.entity.Post;
+import com.kalai.App.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
 public class PostOrderingController {
+
+    @Autowired
+    PostService postService;
+
+    @PostMapping(value="sortPost")
+    public String sortPost(Model model, @RequestParam("sortBy") String sortBy){
+       List<Post> sortedPost=postService.sortPostBy(sortBy);
+       model.addAttribute("posts",sortedPost);
+        return "listofpost";
+    }
+    @PostMapping(value="searchPost")
+    public String searchPost(Model model){
+        return "listofpost";
+    }
+    @PostMapping(value="filerPost")
+    public String filerPost(Model model){
+        return "listofpost";
+    }
 }
